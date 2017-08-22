3f8ba9469d8d93752a7d341801ac48fb647bfa90
QmLbNkUbWQ <automate1@wavemaker.com>
2017-08-22 13:38:25 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_SvpuoaPora",
+  "homePage" : "Main",
+  "name" : "Auto_SvpuoaPora",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
